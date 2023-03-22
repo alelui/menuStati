e86ff570cc4174eda37a79cbff82bd44d7cbb0f7
Alessandro <luisialessandro95@gmail.com>
2023-03-22 22:53:12 +0100

    refactor

--- main.js
+++ main.js
@@ -49,13 +49,10 @@ function getIndiceStato() { return offertaJson.indiceStatoCorrente }
 function changeState(jIndex) {
     const stati = offertaJson.stato;
     const ball = document.querySelectorAll('.ball');
+    //Quando clicco sull'ultimo elemento, disabilito e nascondo bottone
     if (jIndex == stati.length - 2) {
-        /*         list[jIndex].classList.remove('active');
-                list[0].classList.add('active');
-                offertaJson.indiceStatoCorrente = 0;
-                return */
         changeBtn.disabled = true;
-
+        changeBtn.classList.add('display_none');
     }
     if (jIndex < stati.length) {
         list[jIndex].classList.remove('active');
@@ -71,7 +68,9 @@ function fillList() {
     const stati = offertaJson.stato;
     let item = "";
     stati.forEach((element, index) => {
-        item += ` <li><p class="ball ${index == 0 ? "circle_past" : "circle_next"}"></p>${element}</li>`
+        item += `<li>
+                    <p class="ball ${index == 0 ? "circle_past" : "circle_next"}"></p>${element}
+                </li>`
     });
     return item;
 }
